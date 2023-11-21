0678d6d24ea501a68b6ff0e25410ebe69f6c2526
paolomarroq <epmarroquinm@correo.url.edu.gt>
2023-11-20 19:36:17 -0600

    Add project files.

---
+++ ArbolBinario2/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ArbolBinario2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPadre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnPostorden = new System.Windows.Forms.Button();
+            this.btnPreorden = new System.Windows.Forms.Button();
+            this.btnInorden = new System.Windows.Forms.Button();
+            this.txtOrden = new System.Windows.Forms.TextBox();
+            this.btnCalcularNivel = new System.Windows.Forms.Button();
+            this.heigthbttn = new System.Windows.Forms.Button();
+            this.leafbttn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor: ";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(119, 116);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 22);
+            this.txtValor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hijo de:";
+            // 
+            // txtPadre
+            // 
+            this.txtPadre.Location = new System.Drawing.Point(119, 165);
+            this.txtPadre.Name = "txtPadre";
+            this.txtPadre.Size = new System.Drawing.Size(100, 22);
+            this.txtPadre.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(76, 212);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(92, 27);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnPostorden
+            // 
+            this.btnPostorden.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPostorden.Location = new System.Drawing.Point(52, 291);
+            this.btnPostorden.Name = "btnPostorden";
+            this.btnPostorden.Size = new System.Drawing.Size(116, 30);
+            this.btnPostorden.TabIndex = 5;
+            this.btnPostorden.Text = "Post-Order";
+            this.btnPostorden.UseVisualStyleBackColor = true;
+            this.btnPostorden.Click += new System.EventHandler(this.btnPostorden_Click);
+            // 
+            // btnPreorden
+            // 
+            this.btnPreorden.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPreorden.Location = new System.Drawing.Point(295, 291);
+            this.btnPreorden.Name = "btnPreorden";
+            this.btnPreorden.Size = new System.Drawing.Size(115, 30);
+            this.btnPreorden.TabIndex = 6;
+            this.btnPreorden.Text = "Pre-Order";
+            this.btnPreorden.UseVisualStyleBackColor = true;
+            this.btnPreorden.Click += new System.EventHandler(this.btnPreorden_Click);
+            // 
+            // btnInorden
+            // 
+            this.btnInorden.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInorden.Location = new System.Drawing.Point(174, 291);
+            this.btnInorden.Name = "btnInorden";
+            this.btnInorden.Size = new System.Drawing.Size(115, 30);
+            this.btnInorden.TabIndex = 7;
+            this.btnInorden.Text = "In-Order";
+            this.btnInorden.UseVisualStyleBackColor = true;
+            this.btnInorden.Click += new System.EventHandler(this.btnInorden_Click);
+            // 
+            // txtOrden
+            // 
+            this.txtOrden.Location = new System.Drawing.Point(53, 352);
+            this.txtOrden.Name = "txtOrden";
+            this.txtOrden.Size = new System.Drawing.Size(270, 22);
+            this.txtOrden.TabIndex = 8;
+            // 
+            // btnCalcularNivel
+            // 
+            this.btnCalcularNivel.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcularNivel.Location = new System.Drawing.Point(53, 401);
+            this.btnCalcularNivel.Name = "btnCalcularNivel";
+            this.btnCalcularNivel.Size = new System.Drawing.Size(115, 26);
+            this.btnCalcularNivel.TabIndex = 9;
+            this.btnCalcularNivel.Text = "Nivel";
+            this.btnCalcularNivel.UseVisualStyleBackColor = true;
+            this.btnCalcularNivel.Click += new System.EventHandler(this.btnCalcularNivel_Click);
+            // 
+            // heigthbttn
+            // 
+            this.heigthbttn.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heigthbttn.Location = new System.Drawing.Point(174, 401);
+            this.heigthbttn.Name = "heigthbttn";
+            this.heigthbttn.Size = new System.Drawing.Size(115, 26);
+            this.heigthbttn.TabIndex = 10;
+            this.heigthbttn.Text = "Altura";
+            this.heigthbttn.UseVisualStyleBackColor = true;
+            this.heigthbttn.Click += new System.EventHandler(this.heightbutton_click);
+            // 
+            // leafbttn
+            // 
+            this.leafbttn.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leafbttn.Location = new System.Drawing.Point(295, 401);
+            this.leafbttn.Name = "leafbttn";
+            this.leafbttn.Size = new System.Drawing.Size(115, 26);
+            this.leafbttn.TabIndex = 11;
+            this.leafbttn.Text = "Nodos Hojas";
+            this.leafbttn.UseVisualStyleBackColor = true;
+            this.leafbttn.Click += new System.EventHandler(this.leafbttn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(208, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(481, 40);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "PROYECTO FINAL DISCRETA II";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 506);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.leafbttn);
+            this.Controls.Add(this.heigthbttn);
+            this.Controls.Add(this.btnCalcularNivel);
+            this.Controls.Add(this.txtOrden);
+            this.Controls.Add(this.btnInorden);
+            this.Controls.Add(this.btnPreorden);
+            this.Controls.Add(this.btnPostorden);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtPadre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arbol Binario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPadre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnPostorden;
+        private System.Windows.Forms.Button btnPreorden;
+        private System.Windows.Forms.Button btnInorden;
+        private System.Windows.Forms.TextBox txtOrden;
+        private System.Windows.Forms.Button btnCalcularNivel;
+        private System.Windows.Forms.Button heigthbttn;
+        private System.Windows.Forms.Button leafbttn;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ArbolBinario2/Form1.cs
@@ -0,0 +1,463 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArbolBinario2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        string dato = "";
+        ArbolB miArbol = new ArbolB(null);
+        Graphics g;
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtValor.Text == "")
+            {
+                MessageBox.Show("Ingrese un valor");
+            }
+            else
+            {
+                dato = txtValor.Text;
+                if (miArbol.raiz == null)
+                {
+                    miArbol.Insertar(dato);
+                }
+                else
+                {
+                    string padre = txtPadre.Text;
+                    miArbol.InsertarDesdeTextBox(dato, padre);
+                }
+                txtValor.Clear();
+                txtValor.Focus();
+                Invalidate();
+            }
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(this.BackColor);
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g = e.Graphics;
+
+            miArbol.dibujarArbol(g, this.Font, Brushes.White, Brushes.Black, Pens.Black, Brushes.White);
+        }
+
+        private void btnPostorden_Click(object sender, EventArgs e)
+        {
+            if (miArbol.raiz != null)
+            {
+                string postorden = ObtenerPostorden(miArbol.raiz);
+                MessageBox.Show("Recorrido Postorden: " + postorden);
+            }
+            else
+            {
+                MessageBox.Show("El árbol está vacío.");
+            }
+        }
+
+        private string ObtenerPostorden(Nodo nodo)
+        {
+            if (nodo == null)
+            {
+                return string.Empty;
+            }
+
+            string resultado = ObtenerPostorden(nodo.Izquierdo);
+            resultado += ObtenerPostorden(nodo.Derecho);
+            resultado += nodo.valor + " ";
+
+            return resultado;
+        }
+
+
+        private void btnInorden_Click(object sender, EventArgs e)
+        {
+            if (miArbol.raiz != null)
+            {
+                string inorden = ObtenerInorden(miArbol.raiz);
+                MessageBox.Show("Recorrido Inorden: " + inorden);
+            }
+            else
+            {
+                MessageBox.Show("El árbol está vacío.");
+            }
+        }
+
+        private string ObtenerInorden(Nodo nodo)
+        {
+            if (nodo == null)
+            {
+                return string.Empty;
+            }
+
+            string resultado = ObtenerInorden(nodo.Izquierdo);
+            resultado += nodo.valor + " ";
+            resultado += ObtenerInorden(nodo.Derecho);
+
+            return resultado;
+        }
+
+        private void btnPreorden_Click(object sender, EventArgs e)
+        {
+            if (miArbol.raiz != null)
+            {
+                string preorden = ObtenerPreorden(miArbol.raiz);
+                MessageBox.Show("Recorrido Preorden: " + preorden);
+            }
+            else
+            {
+                MessageBox.Show("El árbol está vacío.");
+            }
+        }
+
+        private string ObtenerPreorden(Nodo nodo)
+        {
+            if (nodo == null)
+            {
+                return string.Empty;
+            }
+
+            string resultado = nodo.valor + " ";
+            resultado += ObtenerPreorden(nodo.Izquierdo);
+            resultado += ObtenerPreorden(nodo.Derecho);
+
+            return resultado;
+        }
+
+        private void btnCalcularNivel_Click(object sender, EventArgs e)
+        {
+            string valor = txtOrden.Text;
+            if (string.IsNullOrEmpty(valor))
+            {
+                MessageBox.Show("Por favor, ingrese un valor de nodo para calcular su nivel.");
+                return;
+            }
+
+            int nivel = miArbol.raiz?.CalcularNivel(valor) ?? -1;
+
+            if (nivel != -1)
+            {
+                MessageBox.Show($"El nivel del nodo con valor '{valor}' es: {nivel}");
+            }
+            else
+            {
+                MessageBox.Show("Nodo no encontrado en el árbol.");
+            }
+        }
+
+        private void heightbutton_click(object sender, EventArgs e)
+        {
+            if (miArbol.raiz != null)
+            {
+                int height = CalculateTreeHeight(miArbol.raiz);
+                MessageBox.Show($"La altura del Àrbol es: {height}");
+            }
+            else
+            {
+                MessageBox.Show("El Árbol esta vacìo.");
+            }
+        }
+
+        private int CalculateTreeHeight(Nodo nodo)
+        {
+            if (nodo == null)
+            {
+                return 0;
+            }
+
+            int leftHeight = CalculateTreeHeight(nodo.Izquierdo);
+            int rightHeight = CalculateTreeHeight(nodo.Derecho);
+
+            // The height of the tree is the maximum height of its subtrees plus 1
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+
+        private void leafbttn_Click(object sender, EventArgs e)
+        {
+            if (miArbol.raiz != null)
+            {
+                List<string> leafNodes = miArbol.GetLeafNodes();
+
+                if (leafNodes.Count > 0)
+                {
+                    string leafNodesMessage = string.Join(", ", leafNodes);
+                    MessageBox.Show($"Nodos Hojas: {leafNodesMessage}");
+                }
+                else
+                {
+                    MessageBox.Show("El Àrbol no tiene nodos hojas.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El Árbol esta vacío.");
+            }
+        }
+    }
+
+    class Nodo
+    {
+        public string valor;
+        public Nodo Izquierdo;
+        public Nodo Derecho;
+        public Nodo Padre;
+        public Rectangle nodo;
+
+        private ArbolB arbol;
+
+        public Nodo() { }
+        public ArbolB Arbol
+        {
+            get { return arbol; }
+            set { arbol = value; }
+        }
+        public Nodo(string nuevovalor, Nodo nuevoizquierdo, Nodo nuevoderecho, Nodo nuevopadre)
+        {
+            valor = nuevovalor;
+            Izquierdo = nuevoizquierdo;
+            Derecho = nuevoderecho;
+            Padre = nuevopadre;
+        }
+
+
+
+
+        private const int radio = 30;
+        private const int distanciaH = 80;
+        private const int distanciaV = 10;
+
+        private int CoordenadaX;
+        private int CoordenadaY;
+
+        public void posNodo(ref int xmin, int ymin)
+        {
+            int aux1, aux2;
+            CoordenadaY = (int)(ymin + radio / 2);
+            if (Izquierdo != null)
+            {
+                Izquierdo.posNodo(ref xmin, ymin + radio + distanciaV);
+            }
+            if ((Izquierdo != null) && (Derecho != null))
+            {
+                xmin += distanciaH;
+            }
+            if (Derecho != null)
+            {
+                Derecho.posNodo(ref xmin, ymin + radio + distanciaV);
+            }
+            if (Izquierdo != null && Derecho != null)
+            {
+                CoordenadaX = (int)((Izquierdo.CoordenadaX + Derecho.CoordenadaX) / 2);
+            }
+            else if (Izquierdo != null)
+            {
+                aux1 = Izquierdo.CoordenadaX;
+                Izquierdo.CoordenadaX = CoordenadaX - 80;
+                CoordenadaX = aux1;
+            }
+            else if (Derecho != null)
+            {
+                aux2 = Derecho.CoordenadaX;
+                Derecho.CoordenadaX = CoordenadaX + 80;
+                CoordenadaX = aux2;
+            }
+            else
+            {
+                CoordenadaX = (int)(xmin + radio / 2);
+                xmin += radio;
+            }
+        }
+        public void Ramas(Graphics graf, Pen lapiz)
+        {
+            if (Izquierdo != null)
+            {
+                graf.DrawLine(lapiz, CoordenadaX, CoordenadaY, Izquierdo.CoordenadaX, Izquierdo.CoordenadaY);
+                Izquierdo.Ramas(graf, lapiz);
+            }
+            if (Derecho != null)
+            {
+                graf.DrawLine(lapiz, CoordenadaX, CoordenadaY, Derecho.CoordenadaX, Derecho.CoordenadaY);
+                Derecho.Ramas(graf, lapiz);
+            }
+        }
+
+        public void dibujarNodo(Graphics graf, Font fuente, Brush relleno, Brush rellenoFuente, Pen lapiz, Brush encuentro)
+        {
+            Rectangle rect = new Rectangle((int)(CoordenadaX - radio / 2), (int)(CoordenadaY - radio / 2), radio, radio);
+            graf.FillEllipse(encuentro, rect);
+            graf.FillEllipse(relleno, rect);
+            graf.DrawEllipse(lapiz, rect);
+            StringFormat formato = new StringFormat();
+            formato.Alignment = StringAlignment.Center;
+            formato.LineAlignment = StringAlignment.Center;
+            graf.DrawString(valor.ToString(), fuente, rellenoFuente, CoordenadaX, CoordenadaY, formato);
+            if (Izquierdo != null)
+            {
+                Izquierdo.dibujarNodo(graf, fuente, relleno, rellenoFuente, lapiz, encuentro);
+            }
+            if (Derecho != null)
+            {
+                Derecho.dibujarNodo(graf, fuente, relleno, rellenoFuente, lapiz, encuentro);
+            }
+        }
+        public int CalcularNivel(string valor, int nivel = 1)
+        {
+            if (this.valor == valor)
+            {
+                return nivel;
+            }
+
+            int nivelIzquierdo = Izquierdo?.CalcularNivel(valor, nivel + 1) ?? -1;
+            int nivelDerecho = Derecho?.CalcularNivel(valor, nivel + 1) ?? -1;
+
+            return Math.Max(nivelIzquierdo, nivelDerecho);
+        }
+    }
+    class ArbolB
+    {
+        public Nodo raiz;
+        public Nodo aux;
+        public ArbolB()
+        {
+            aux = new Nodo();
+        }
+        public ArbolB(Nodo nuevaraiz)
+        {
+            raiz = nuevaraiz;
+        }
+        public void Insertar(string x)
+        {
+            Nodo nuevo = new Nodo(x, null, null, null);
+            if (raiz == null)
+            {
+                this.raiz = nuevo;
+            }
+            else
+            {
+                raiz = this.InsertarNodo(raiz, nuevo);
+            }
+        }
+        public Nodo InsertarNodo(Nodo actual, Nodo nuevo)
+        {
+            if (nuevo.valor.CompareTo(actual.valor) < 0)
+            {
+                if (actual.Izquierdo == null)
+                {
+                    actual.Izquierdo = nuevo;
+                    return actual;
+                }
+                else
+                {
+                    actual.Izquierdo = InsertarNodo(actual.Izquierdo, nuevo);
+                    return actual;
+                }
+            }
+            else if (nuevo.valor.CompareTo(actual.valor) > 0)
+            {
+                if (actual.Derecho == null)
+                {
+                    actual.Derecho = nuevo;
+                    return actual;
+                }
+                else
+                {
+                    actual.Derecho = InsertarNodo(actual.Derecho, nuevo);
+                    return actual;
+                }
+            }
+            else
+            {
+                return actual;
+            }
+        }
+        public void InsertarDesdeTextBox(string nuevoValor, string valorPadre)
+        {
+            Nodo nodoPadre = EncontrarNodo(this.raiz, valorPadre);
+
+            if (nodoPadre != null)
+            {
+                Nodo nuevoNodo = new Nodo(nuevoValor, null, null, nodoPadre);
+
+                if (nodoPadre.Izquierdo == null)
+                {
+                    nodoPadre.Izquierdo = nuevoNodo;
+                }
+                else if (nodoPadre.Derecho == null)
+                {
+                    nodoPadre.Derecho = nuevoNodo;
+                }
+                else
+                {
+                    MessageBox.Show("El nodo padre ya tiene ambos hijos.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El nodo padre no se encontró en el árbol.");
+            }
+        }
+        public Nodo EncontrarNodo(Nodo nodo, string valor)
+        {
+            if (nodo == null || nodo.valor == valor)
+            {
+                return nodo;
+            }
+
+            Nodo izquierda = EncontrarNodo(nodo.Izquierdo, valor);
+            Nodo derecha = EncontrarNodo(nodo.Derecho, valor);
+
+            return izquierda ?? derecha;
+        }
+        public void dibujarArbol(Graphics graf, Font funete, Brush relleno, Brush rellenoFuente, Pen lapiz, Brush encuentro)
+        {
+            int x = 500;
+            int y = 100;
+            if (raiz != null)
+            {
+                raiz.posNodo(ref x, y);
+                raiz.Ramas(graf, lapiz);
+                raiz.dibujarNodo(graf, funete, relleno, rellenoFuente, lapiz, encuentro);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public List<string> GetLeafNodes()
+        {
+            List<string> leafNodes = new List<string>();
+            CollectLeafNodes(raiz, leafNodes);
+            return leafNodes;
+        }
+
+        private void CollectLeafNodes(Nodo nodo, List<string> leafNodes)
+        {
+            if (nodo != null)
+            {
+                if (nodo.Izquierdo == null && nodo.Derecho == null)
+                {
+                    leafNodes.Add(nodo.valor);
+                }
+
+                CollectLeafNodes(nodo.Izquierdo, leafNodes);
+                CollectLeafNodes(nodo.Derecho, leafNodes);
+            }
+        }
+    }
+}
